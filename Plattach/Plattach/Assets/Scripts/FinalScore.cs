17245a3a13a1a8f6dee1530f8da280f89081e88e
wlgp8563 <63843762+wlgp8563@users.noreply.github.com>
2023-05-29 00:45:49 +0900

    UI
    UI

---
+++ Plattach/Plattach/Assets/Scripts/FinalScore.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour {
+
+    public GameObject scoreManagerObject;
+    private ScoreManager scoreManager;
+    private ScoreCounter score_counter = null;
+    //public Text finalScoreText;
+    public GUIStyle guistyle; // 폰트 스타일.;
+    public int finalScore;
+    public int finalLeftMoves;
+    public int realFinalScore;
+
+    //[SerializeField]
+    //public Vector3 textPosition; // 텍스트의 위치를 조정하기 위한 변수
+    //public float textSize; // 텍스트의 크기를 조정하기 위한 변수*/
+
+    public GameObject oneStar;
+    public GameObject twoStar;
+    public GameObject threeStar;
+    public GameObject canvas;
+
+    public int oneStarScore;
+    public int twoStarScore;
+    public int threeStarScore;
+
+    private GameObject scorePannel;
+
+    void Start()
+    {
+        scoreManagerObject = GameObject.Find("ScoreManager");
+        this.scoreManager = scoreManagerObject.GetComponent<ScoreManager>();
+        finalScore = scoreManager.GetCurrentScore();
+        finalLeftMoves = scoreManager.GetCurrentMoves();
+        realFinalScore = finalScore + finalLeftMoves * 1000; //여기서 진짜 최종 점수 합산
+
+        
+
+        if(realFinalScore>= threeStarScore)
+        {
+            scorePannel =
+                    Instantiate(this.threeStar, canvas.transform) as GameObject;
+        }
+        else if(realFinalScore >= twoStarScore)
+        {
+            scorePannel =
+                   Instantiate(this.twoStar, canvas.transform) as GameObject;
+        }
+        else if (realFinalScore >= oneStarScore)
+        {
+            scorePannel =
+                   Instantiate(this.oneStar, canvas.transform) as GameObject;
+        }
+    }
+
+    void Update() 
+	{
+
+        //finalScoreText.text = "Final_Score: " + finalScore.ToString();
+
+        //finalScoreText.transform.position = textPosition;
+
+        // 텍스트 폰트 변경
+        //finalScoreText.font = textFont;
+        //finalScoreText.fontSize = Mathf.RoundToInt(textSize);
+    }
+    void OnGUI()
+    {
+        int x = 1300 / 2 +50;
+        int y = 600 / 2 +125;
+        GUI.color = Color.black;
+        this.print_value(x, y, "1, 2 라운드 합산 점수", finalScore);
+        y += 60;
+        this.print_value(x, y, "남은 이동 횟수", finalLeftMoves);
+        y += 60;
+        this.print_value(x, y, "최종 합산 점수", realFinalScore);
+    }
+    public void print_value(int x, int y, string label, float value)
+    {
+        this.guistyle.fontSize = 25;
+        // label을 표시.
+        GUI.Label(new Rect(x, y, 100, 20), label, guistyle);
+        // 다음 행에 value를 표시.
+        GUI.Label(new Rect(x + 175, y, 100, 20), value.ToString(), guistyle);
+    }
+}
+
---
+++ Plattach/Plattach/Assets/Scripts/MoveCounter.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCounter : MonoBehaviour
+{
+	[SerializeField]
+	private int limitMoves; // 최대 이동 가능 횟수
+	private int leftMoves; // 현재 남은 이동 가능 횟수
+
+	public GUIStyle guistyle; // 폰트 스타일.
+
+	private BlockRoot block_root = null;
+	public GameObject scoreManagerObject;
+	private ScoreManager scoreManager;
+	private int moves;
+	void Start()
+	{
+		scoreManagerObject = GameObject.Find("ScoreManager");
+		this.scoreManager = scoreManagerObject.GetComponent<ScoreManager>();
+		this.block_root = this.gameObject.GetComponent<BlockRoot>();
+		this.leftMoves = this.limitMoves + this.scoreManager.GetCurrentMoves();
+		moves = 0;
+		this.guistyle.fontSize = 30;
+	}
+
+	void OnGUI()
+	{
+		this.guistyle.fontSize = 30;
+		int x = 0;
+		int y = 390;
+		GUI.color = Color.red;
+		this.print_value(x + 140, y, "남은 이동 횟수: ", this.leftMoves);
+	}
+
+	public void print_value(int x, int y, string label, int value)
+	{
+		// label을 표시.
+		GUI.Label(new Rect(x, y, 100, 20), label, guistyle);
+		//y += 30;
+		// 다음 행에 value를 표시.
+		GUI.Label(new Rect(x + 170, y, 100, 20), value.ToString(), guistyle);
+		y += 30;
+	}
+	public void minusLeftMoves()
+	{
+		this.leftMoves--;
+		moves++;
+	}
+	public void plusLeftMoves()
+	{
+		this.leftMoves++;
+	}
+	public int getLeftMoves() //남은 이동 가능 횟수를 리턴
+	{
+		return this.leftMoves;
+	}
+	public int getMoves() //현재 라운드에서 이 함수가 출력한 시점 까지의 총 이동한 횟수를 리턴
+	{
+		return moves;
+	}
+
+	public bool isLeftMovesZero()
+	{
+		
+		if (this.leftMoves > 0)
+			return false;
+		// 현재 남은 이동 횟수가 0이면 
+		return true;
+	}
+}
